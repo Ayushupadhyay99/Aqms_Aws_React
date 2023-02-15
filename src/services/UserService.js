1690b6a04dabf9c68709ae6d13a31253a2cbb093
Ayush upadhyay <aayush9852@gmail.com>
2023-02-15 15:53:27 +0530

    updated-code-push

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const USERS_REST_API_URL = 'http://aqmsaws-env.eba-p4pdeysp.ap-northeast-1.elasticbeanstalk.com/api/v1/CompleteDatalist';
-const USERS_REST_API_URL1 = 'http://aqmsaws-env.eba-p4pdeysp.ap-northeast-1.elasticbeanstalk.com/api/v1/CarbonMonoxideData';
+const USERS_REST_API_URL1 = 'http://aqmsaws-env.eba-p4pdeysp.ap-northeast-1.elasticbeanstalk.com/api/v1/CarbonDioxideData';
 const USERS_REST_API_URL2 = 'http://aqmsaws-env.eba-p4pdeysp.ap-northeast-1.elasticbeanstalk.com/api/v1/dustparticalData';
 const SEND_EMAIL = 'http://aqmsaws-env.eba-p4pdeysp.ap-northeast-1.elasticbeanstalk.com/api/v1/sendEmail';
 
